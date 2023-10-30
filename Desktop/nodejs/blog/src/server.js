f8b1cf09bff8ec9af99e33860b6c1df2b7bf0376
Long <laptoptandt@Longs-Macbookpro.local>
2023-10-30 15:37:34 +0700

    30/10/2023

--- Desktop/nodejs/blog/src/server.js
+++ Desktop/nodejs/blog/src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8888  // port
 const hostname = process.env.HOST_NAME
 const configViewEngine = require('./config/viewEngine')
 const webRouter = require('./routes/web')
-const mysql = require('mysql2');
+const connection = require('./config/database')
 
 // config template engine
 configViewEngine(app)
@@ -16,22 +16,11 @@ configViewEngine(app)
 app.use('/', webRouter) //neu them link '/xxx' vao thi tat ca cai route khai bao se dung sau duong link nay
 
 
-// test connection
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'Teo151104'
-});
-
 // simple query
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
         console.log("results: ", results); // results contains rows returned by server
-        console.log("fields: ", fields); // fields contains extra meta data about results, if available
     }
 );
 
